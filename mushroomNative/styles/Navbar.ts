62381e8fd8191eaf709239be871401452864bbd1
mohammadjavad <mjkmo123@gmail.com>
2021-10-13 14:29:11 +0330

    fix

--- mushroomNative/styles/Navbar.ts
+++ mushroomNative/styles/Navbar.ts
@@ -3,7 +3,7 @@ import {StyleSheet} from "react-native";
 export const navbarStyle = StyleSheet.create({
     container: {
         width: '100%',
-        height: 70,
+        height: 60,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-evenly',
