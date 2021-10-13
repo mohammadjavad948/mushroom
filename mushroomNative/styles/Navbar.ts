0a1eb2bee2581e68de7ff728fc3e8955d05c6ffd
mohammadjavad <mjkmo123@gmail.com>
2021-10-13 14:12:15 +0330

    fix

--- mushroomNative/styles/Navbar.ts
+++ mushroomNative/styles/Navbar.ts
@@ -3,7 +3,7 @@ import {StyleSheet} from "react-native";
 export const navbarStyle = StyleSheet.create({
     container: {
         width: '100%',
-        height: 60,
+        height: 70,
         backgroundColor: 'red',
     }
 })
