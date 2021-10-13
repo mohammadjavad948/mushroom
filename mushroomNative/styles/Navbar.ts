d7ff0c8febf8fd0993c14d647616d30a153c2623
mohammadjavad <mjkmo123@gmail.com>
2021-10-13 14:13:25 +0330

    style

--- mushroomNative/styles/Navbar.ts
+++ mushroomNative/styles/Navbar.ts
@@ -4,6 +4,9 @@ export const navbarStyle = StyleSheet.create({
     container: {
         width: '100%',
         height: 70,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
         backgroundColor: '#3b3b3b',
     }
 })
