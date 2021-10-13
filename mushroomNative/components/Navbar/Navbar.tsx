e962d6afbb5169fafcd9b1dcd6347eb730ccf922
mohammadjavad <mjkmo123@gmail.com>
2021-10-13 18:25:21 +0330

    nice

--- mushroomNative/components/Navbar/Navbar.tsx
+++ mushroomNative/components/Navbar/Navbar.tsx
@@ -6,19 +6,35 @@ import NavbarItem from "./NavbarItem";
 
 export default function NavBar(){
 
+    function click(){
+        console.log('click!')
+    }
 
     return (
         <View style={navbarStyle.container}>
-            <NavbarItem route={'/'} exact={true}>
+            <NavbarItem
+                click={click}
+                route={'/'}
+                exact={true}
+            >
                 <Icon name="dashboard" size={25}/>
             </NavbarItem>
-            <NavbarItem route={'/add'}>
+            <NavbarItem
+                click={click}
+                route={'/add'}
+            >
                 <Icon name="add" size={25}/>
             </NavbarItem>
-            <NavbarItem route={'/subject'}>
+            <NavbarItem
+                click={click}
+                route={'/subject'}
+            >
                 <Icon name="layers" size={25}/>
             </NavbarItem>
-            <NavbarItem route={'/info'}>
+            <NavbarItem
+                click={click}
+                route={'/info'}
+            >
                 <Icon name="person" size={25}/>
             </NavbarItem>
         </View>
