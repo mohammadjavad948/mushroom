cff29a8132c9fb8a002644b05aeafda61e07db51
mohammadjavad <mjkmo123@gmail.com>
2021-10-13 14:14:18 +0330

    dummy text

--- mushroomNative/components/Navbar/Navbar.tsx
+++ mushroomNative/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View} from "react-native";
+import {Text, View} from "react-native";
 import {navbarStyle} from "../../styles/Navbar";
 
 export default function NavBar(){
@@ -7,7 +7,9 @@ export default function NavBar(){
 
     return (
         <View style={navbarStyle.container}>
-
+            <Text>1</Text>
+            <Text>2</Text>
+            <Text>3</Text>
         </View>
     )
 }
