ad1a811f81fa8ebee3f2ecbe54f6c65f778b8767
mohammadjavad <mjkmo123@gmail.com>
2021-10-13 21:14:08 +0330

    profile

---
+++ mushroomNative/components/Profile/Profile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {View} from "react-native";
+import {profileStyle} from "../../styles/Profile";
+
+export default function Profile(){
+
+    return (
+        <View style={profileStyle.container}>
+
+        </View>
+    )
+}
