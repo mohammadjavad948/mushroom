6c3ba7a0914cd2cb7a844382df3f12d31e2ed799
mohammadjavad <mjkmo123@gmail.com>
2021-10-13 14:00:27 +0330

    nav

---
+++ mushroomNative/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import {View} from "react-native";
+import {navbarStyle} from "../styles/Navbar";
+
+export default function NavBar(){
+
+
+    return (
+        <View style={navbarStyle.container}>
+
+        </View>
+    )
+}
---
+++ mushroomNative/styles/Navbar.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from "react-native";
+
+export const navbarStyle = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 40,
+        backgroundColor: 'red',
+    }
+})
