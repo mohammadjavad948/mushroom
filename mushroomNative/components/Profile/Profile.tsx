3e5db6d23f93d424963f5e83b5467054ccce9f99
mohammadjavad <mjkmo123@gmail.com>
2021-10-14 10:02:33 +0330

    info

--- mushroomNative/components/Profile/Profile.tsx
+++ mushroomNative/components/Profile/Profile.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import {View} from "react-native";
+import {ScrollView, Text, View} from "react-native";
 import {profileStyle} from "../../styles/Profile";
 
 export default function Profile(){
 
     return (
-        <View style={profileStyle.container}>
-
-        </View>
+        <ScrollView style={profileStyle.container}>
+            <View style={profileStyle.view}>
+                <Text style={profileStyle.title}>Account info</Text>
+                <Text style={profileStyle.username}>Name</Text>
+            </View>
+        </ScrollView>
     )
 }
--- mushroomNative/styles/Profile.ts
+++ mushroomNative/styles/Profile.ts
@@ -3,5 +3,18 @@ import {StyleSheet} from "react-native";
 export const profileStyle = StyleSheet.create({
     container: {
         flex: 1
+    },
+    title: {
+      fontSize: 18,
+      fontWeight: "300",
+      color: 'white'
+    },
+    view: {
+        padding: 15,
+        width: '100%'
+    },
+    username: {
+        fontSize: 15,
+        marginTop: 10
     }
 })
