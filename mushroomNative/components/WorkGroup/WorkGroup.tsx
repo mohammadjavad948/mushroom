a23af1794aa5a628ce40c5f1e214928f1b755807
mohammadjavad <mjkmo123@gmail.com>
2021-10-14 10:09:57 +0330

    workGroup

---
+++ mushroomNative/components/WorkGroup/WorkGroup.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {View} from "react-native";
+import {workGroupStyle} from "../../styles/WorkGroup";
+
+export default function WorkGroup(){
+
+    return (
+        <View style={workGroupStyle.container}>
+
+        </View>
+    )
+}
