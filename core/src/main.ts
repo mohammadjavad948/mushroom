a015059175e344f363eacce4cd4567f67f3f76fe
mohammadjavad <mjkmo123@gmail.com>
2021-10-14 22:02:56 +0330

    enable versioning

--- core/src/main.ts
+++ core/src/main.ts
@@ -4,9 +4,15 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import {VersioningType} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
