01f5d9b3dccf1cbbab8f8391a5b8441b45ffd646
mohammadjavad <mjkmo123@gmail.com>
2021-10-15 10:43:58 +0330

    can

--- core/src/work-group/work-group.service.ts
+++ core/src/work-group/work-group.service.ts
@@ -54,6 +54,17 @@ export class WorkGroupService {
     return `This action removes a #${id} workGroup`;
   }
 
+  async canManageGroup(userId: number, groupId: number){
+    const group = await this.database.workGroup.count({
+      where: {
+        id: groupId,
+        creatorId: userId,
+      }
+    });
+
+    return group > 0
+  }
+
   async canViewGroup(userId: number, groupId: number){
     const sub = await this.database.subscription.count({
       where: {
