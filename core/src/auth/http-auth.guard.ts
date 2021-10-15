c7830a482fcbf6a469fd770cc6f843b6360ee9cf
mohammadjavad <mjkmo123@gmail.com>
2021-10-15 09:49:16 +0330

    inject

--- core/src/auth/http-auth.guard.ts
+++ core/src/auth/http-auth.guard.ts
@@ -1,7 +1,10 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {DatabaseService} from "../database/database.service";
+import {AuthService} from "./auth.service";
 
 @Injectable()
 export class HttpAuthGuard implements CanActivate {
+  constructor(private database: DatabaseService, private auth: AuthService) {}
   async canActivate(
     context: ExecutionContext,
   ): Promise<boolean> {
