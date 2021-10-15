177c5b00b57335200e233a411feabe51e473a6ff
mohammadjavad <mjkmo123@gmail.com>
2021-10-15 08:12:07 +0330

    compare

--- core/src/auth/auth.service.ts
+++ core/src/auth/auth.service.ts
@@ -3,13 +3,22 @@ import {DatabaseService} from "../database/database.service";
 import {sign, verify} from "jsonwebtoken";
 import {ConfigService} from "@nestjs/config";
 import {TokenPayloadI} from "../types/token";
+import {compare} from "bcrypt";
 
 @Injectable()
 export class AuthService {
     constructor(private database: DatabaseService,  private config: ConfigService) { }
 
-    async login(token: string){
+    async login(username: string, password: string){
+        const user = await this.database.user.findFirst({
+            where: {
+                username: username,
+            },
+        });
+    }
 
+    checkPassword(password: string, hashedPassword: string){
+        return compare(password, hashedPassword);
     }
 
     generateToken(payload: TokenPayloadI){
