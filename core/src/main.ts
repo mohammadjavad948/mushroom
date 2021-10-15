e80d16a75c8da55813be95ed2e8d1f0d34be386b
mohammadjavad <mjkmo123@gmail.com>
2021-10-15 11:35:53 +0330

    ip

--- core/src/main.ts
+++ core/src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
 
   console.log('port: ', process.env.PORT)
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
