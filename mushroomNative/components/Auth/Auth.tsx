37be30e07d0cd7de64625cd97e09be7bcb8b2f9b
mohammadjavad <mjkmo123@gmail.com>
2021-10-15 12:48:14 +0330

    auth

---
+++ mushroomNative/components/Auth/Auth.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {View} from "react-native";
+import {mainStyle} from "../../styles/Main";
+
+export default function Auth(){
+
+    return (
+        <View
+            style={[
+                mainStyle.main,
+                {
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }
+            ]}
+        >
+
+        </View>
+    )
+}
