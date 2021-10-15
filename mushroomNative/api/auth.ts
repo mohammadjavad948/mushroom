bec4a7e5ebdf2568277dfd7fba1453bc5cf6972e
mohammadjavad <mjkmo123@gmail.com>
2021-10-15 19:04:59 +0330

    info

--- mushroomNative/api/auth.ts
+++ mushroomNative/api/auth.ts
@@ -1,6 +1,20 @@
 import {api} from "./base";
+import {useAuthStore} from "../stores/authStore";
 
 export function login(data: any){
 
     return api.post('/auth/login', data);
+}
+
+export function info(){
+
+    return api.get('/auth/info', {
+        headers: headers()
+    });
+}
+
+export function headers(){
+    return {
+        auth: useAuthStore.getState().token
+    }
 }
