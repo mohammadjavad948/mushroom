a3609bb0b6f511029326a76148f8151bfaa266fa
mohammadjavad <mjkmo123@gmail.com>
2021-10-15 20:08:17 +0330

    style

--- mushroomNative/components/WorkGroup/GroupItem.tsx
+++ mushroomNative/components/WorkGroup/GroupItem.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import {View} from "react-native";
+import {workGroupStyle} from "../../styles/WorkGroup";
 
 export default function GroupItem(){
 
     return (
-        <View>
+        <View style={workGroupStyle.item}>
 
         </View>
     )
--- mushroomNative/styles/WorkGroup.ts
+++ mushroomNative/styles/WorkGroup.ts
@@ -4,4 +4,12 @@ export const workGroupStyle = StyleSheet.create({
     container: {
         flex: 1
     },
+    item: {
+        width: '90%',
+        padding: 20,
+        borderRadius: 10,
+        borderStyle: "solid",
+        borderWidth: 1,
+        borderColor: "white"
+    }
 })
