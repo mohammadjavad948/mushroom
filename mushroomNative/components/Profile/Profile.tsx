28c637eb8ef0667f838648fc1276effafd2c6875
mohammadjavad <mjkmo123@gmail.com>
2021-10-15 19:05:54 +0330

    hmm

--- mushroomNative/components/Profile/Profile.tsx
+++ mushroomNative/components/Profile/Profile.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import {ScrollView, View} from "react-native";
 import {Text} from 'react-native-paper';
 import {profileStyle} from "../../styles/Profile";
+import {useQuery} from "react-query";
+import {info} from "../../api/auth";
 
 export default function Profile(){
+
+    const {data, isLoading} = useQuery('userInfo', info)
+
+    console.log(data?.data)
 
     return (
         <ScrollView style={profileStyle.container}>
