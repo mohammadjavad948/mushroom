1c17376e49c837771a2a17ebdac6960b1eebc8a7
mohammadjavad <mjkmo123@gmail.com>
2021-10-15 13:02:28 +0330

    login

--- mushroomNative/components/Auth/Auth.tsx
+++ mushroomNative/components/Auth/Auth.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {View} from "react-native";
 import {mainStyle} from "../../styles/Main";
-import {Text, TextInput} from "react-native-paper";
+import {Button, Text, TextInput} from "react-native-paper";
 import {authStyle} from "../../styles/Auth";
 
 export default function Auth(){
@@ -29,6 +29,11 @@ export default function Auth(){
                 label={"password"}
                 mode={"outlined"}
             />
+            <Button
+                mode="contained"
+            >
+                Login
+            </Button>
         </View>
     )
 }
