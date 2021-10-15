4234cb5d68a5b476fa19db662bd5199733e2ed48
mohammadjavad <mjkmo123@gmail.com>
2021-10-15 13:39:48 +0330

    formik

--- mushroomNative/components/Auth/Auth.tsx
+++ mushroomNative/components/Auth/Auth.tsx
@@ -7,36 +7,48 @@ import {authStyle} from "../../styles/Auth";
 export default function Auth(){
 
     return (
-        <View
-            style={[
-                mainStyle.main,
-                {
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }
-            ]}
+        <Formik
+            initialValues={{ username: '', password: '' }}
+            onSubmit={values => console.log(values)}
         >
-            <Text style={authStyle.text}>
-                Login
-            </Text>
-            <TextInput
-                style={authStyle.input}
-                label={"username"}
-                mode={"outlined"}
-            />
-            <TextInput
-                style={authStyle.input}
-                label={"password"}
-                mode={"outlined"}
-                type={"password"}
-                secureTextEntry={true}
-            />
-            <Button
-                mode="contained"
-                style={{marginTop: 10}}
-            >
-                Login
-            </Button>
-        </View>
+            {({ handleChange, handleBlur, handleSubmit, values }) => (
+                <View
+                    style={[
+                        mainStyle.main,
+                        {
+                            alignItems: 'center',
+                            justifyContent: 'center'
+                        }
+                    ]}
+                >
+                    <Text style={authStyle.text}>
+                        Login
+                    </Text>
+                    <TextInput
+                        style={authStyle.input}
+                        label={"username"}
+                        mode={"outlined"}
+                        onChangeText={handleChange('username')}
+                        onBlur={handleBlur('username')}
+                    />
+                    <TextInput
+                        style={authStyle.input}
+                        label={"password"}
+                        mode={"outlined"}
+                        type={"password"}
+                        secureTextEntry={true}
+                        onChangeText={handleChange('password')}
+                        onBlur={handleBlur('password')}
+                    />
+                    <Button
+                        mode="contained"
+                        style={{marginTop: 10}}
+                        onPress={handleSubmit}
+                    >
+                        Login
+                    </Button>
+                </View>
+            )}
+        </Formik>
     )
 }
