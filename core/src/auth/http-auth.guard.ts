6005f43a8525e466867481bc912187f6476920cd
mohammadjavad <mjkmo123@gmail.com>
2021-10-15 09:50:24 +0330

    get token

--- core/src/auth/http-auth.guard.ts
+++ core/src/auth/http-auth.guard.ts
@@ -8,6 +8,6 @@ export class HttpAuthGuard implements CanActivate {
   async canActivate(
     context: ExecutionContext,
   ): Promise<boolean> {
-    return true;
+    const token = context.switchToHttp().getRequest().headers?.auth;
   }
 }
