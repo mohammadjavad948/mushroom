13262816333e696d3a126b731e32c6465b940e99
mohammadjavad <mjkmo123@gmail.com>
2021-10-15 07:31:19 +0330

    config

--- core/src/auth/auth.controller.ts
+++ core/src/auth/auth.controller.ts
@@ -1,11 +1,12 @@
 import {Controller, Get} from '@nestjs/common';
 import {AuthService} from './auth.service';
+import {ConfigService} from "@nestjs/config";
 
 @Controller({
   path: 'auth',
 })
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService, private config: ConfigService) {}
 
   @Get('login')
   login(){
