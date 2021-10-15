4df1f40ab7704d92a472c2d45153710438db409f
mohammadjavad <mjkmo123@gmail.com>
2021-10-15 12:56:38 +0330

    token

--- mushroomNative/stores/authStore.ts
+++ mushroomNative/stores/authStore.ts
@@ -4,13 +4,21 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface TokenI{
     token: string
+    setToken: (token: string) => void
+    logout: () => void
 }
 
 export const useAuthStore = create<TokenI>(persist(
     (set, get) => {
 
         return {
-            token: ""
+            token: "",
+            setToken: token => {
+                set({token});
+            },
+            logout: () => {
+                set({token: ""});
+            }
         }
     },
     {
