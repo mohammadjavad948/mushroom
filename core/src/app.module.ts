328dcd59a9689bafd17051e0a73ed6c356737ec3
mohammadjavad <mjkmo123@gmail.com>
2021-10-15 07:30:04 +0330

    config

--- core/src/app.module.ts
+++ core/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseService } from './database/database.service';
 import { AuthModule } from './auth/auth.module';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [AuthModule],
+  imports: [ConfigModule.forRoot(), AuthModule],
   controllers: [AppController],
   providers: [AppService, DatabaseService],
 })
