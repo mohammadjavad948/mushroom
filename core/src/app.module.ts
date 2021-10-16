d1bdd81d4faaf7aa6131b6c980b457308c492755
mohammadjavad <mjkmo123@gmail.com>
2021-10-16 20:42:32 +0330

    helper service

--- core/src/app.module.ts
+++ core/src/app.module.ts
@@ -6,10 +6,11 @@ import {ConfigModule} from "@nestjs/config";
 import { DatabaseModule } from './database/database.module';
 import { WorkGroupModule } from './work-group/work-group.module';
 import { WorkModule } from './work/work.module';
+import { HelperService } from './helper/helper.service';
 
 @Module({
   imports: [ConfigModule.forRoot({isGlobal: true}), AuthModule, DatabaseModule, WorkGroupModule, WorkModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, HelperService],
 })
 export class AppModule {}
