f3cf5202f62168c4233825ca0284a32a5f902f0f
mohammadjavad <mjkmo123@gmail.com>
2021-10-16 20:26:11 +0330

    works base

--- core/src/app.module.ts
+++ core/src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import {ConfigModule} from "@nestjs/config";
 import { DatabaseModule } from './database/database.module';
 import { WorkGroupModule } from './work-group/work-group.module';
+import { WorkModule } from './work/work.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), AuthModule, DatabaseModule, WorkGroupModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), AuthModule, DatabaseModule, WorkGroupModule, WorkModule],
   controllers: [AppController],
   providers: [AppService],
 })
