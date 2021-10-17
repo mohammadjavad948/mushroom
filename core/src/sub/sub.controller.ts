faddc36372ccc00f282f12076085c93c222e98ff
mohammadjavad <mjkmo123@gmail.com>
2021-10-17 09:34:19 +0330

    sub

--- core/src/sub/sub.controller.ts
+++ core/src/sub/sub.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Param, Patch, Req} from '@nestjs/common';
 import { SubService } from './sub.service';
+import {Request} from "../types/request";
 
 @Controller('sub')
 export class SubController {
   constructor(private readonly subService: SubService) {}
+
+  @Patch('/:id')
+  async sub(@Param('id') id: string, @Req() req: Request){
+
+  }
 }
