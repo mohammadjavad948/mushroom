c99e59ed1e7a78e8fe1f69dd8baa495cbe1c74bd
mohammadjavad <mjkmo123@gmail.com>
2021-10-17 19:28:27 +0330

    query

--- core/src/search/search.service.ts
+++ core/src/search/search.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import {DatabaseService} from "../database/database.service";
 
 @Injectable()
-export class SearchService {}
+export class SearchService {
+    constructor(private database: DatabaseService) {}
+
+    async search(query: string){
+        return this.database.workGroup.findMany({
+            where: {
+                name: {
+                    contains: query
+                }
+            }
+        })
+    }
+}
