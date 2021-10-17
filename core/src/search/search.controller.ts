720fb0bfeb18df817c11e347d28d196d4433418d
mohammadjavad <mjkmo123@gmail.com>
2021-10-17 19:27:17 +0330

    get

--- core/src/search/search.controller.ts
+++ core/src/search/search.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, Query} from '@nestjs/common';
 import { SearchService } from './search.service';
 
 @Controller('search')
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
+
+  @Get()
+  async search(@Query('q') query: string){
+
+  }
 }
