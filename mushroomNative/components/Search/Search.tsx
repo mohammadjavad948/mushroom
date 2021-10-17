d48d6cca9877d7329f64e61d49918c9873817040
mohammadjavad <mjkmo123@gmail.com>
2021-10-17 19:58:18 +0330

    search

--- mushroomNative/components/Search/Search.tsx
+++ mushroomNative/components/Search/Search.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import {View} from "react-native";
+import {TextInput} from "react-native-paper";
 
 
 export default function Search(){
 
     return (
         <View
-            style={{flex: 1}}
+            style={{flex: 1, alignItems: 'center'}}
         >
-
+            <TextInput
+                style={{
+                    width: '95%'
+                }}
+                mode={"outlined"}
+                label={"Search"}
+            />
         </View>
     )
 }
