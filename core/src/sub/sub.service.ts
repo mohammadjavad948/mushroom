75611137b9ef5e18aca132b19b61b3013e333095
mohammadjavad <mjkmo123@gmail.com>
2021-10-17 10:55:51 +0330

    subs

--- core/src/sub/sub.service.ts
+++ core/src/sub/sub.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import {DatabaseService} from "../database/database.service";
 
 @Injectable()
-export class SubService {}
+export class SubService {
+    constructor(private database: DatabaseService) {}
+
+    async allSubs(userId: number){
+
+        return this.database.workGroup.findMany({
+            where: {
+                subscribers: {
+                    some: {
+                        userId: userId,
+                    }
+                }
+            }
+        })
+    }
+}
