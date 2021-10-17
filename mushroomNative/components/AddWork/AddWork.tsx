af7cb3c17d84bc626fea8faa2c2b896c7e9a3ddc
mohammadjavad <mjkmo123@gmail.com>
2021-10-17 14:06:05 +0330

    add work

--- mushroomNative/components/AddWork/AddWork.tsx
+++ mushroomNative/components/AddWork/AddWork.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {ScrollView, View} from "react-native";
 import {Text} from 'react-native-paper';
 import {addWorkStyle} from "../../styles/AddWork";
@@ -6,8 +6,21 @@ import {TextInput} from "react-native-paper";
 import {Picker} from '@react-native-picker/picker';
 import {useQuery} from "react-query";
 import {allWorkGroups} from "../../api/workGroup";
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function AddWork(){
+
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [show, setShow] = useState(false);
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setDate(currentDate);
+    };
+
+    const showMode = (currentMode) => {
+        setShow(true);
+    };
 
     const {data} = useQuery(['workGroup'], allWorkGroups)
 
@@ -41,6 +54,14 @@ export default function AddWork(){
                         )
                     })}
                 </Picker>
+                <DateTimePicker
+                    testID="dateTimePicker"
+                    value={date}
+                    mode={"date"}
+                    is24Hour={true}
+                    display="default"
+                    onChange={onChange}
+                />
             </View>
         </ScrollView>
     )
