a18b82e2aad59d31d8b914a053d98e45ae2ccf5c
mohammadjavad <mjkmo123@gmail.com>
2021-10-17 14:12:18 +0330

    submit

--- mushroomNative/components/AddWork/AddWork.tsx
+++ mushroomNative/components/AddWork/AddWork.tsx
@@ -1,29 +1,26 @@
-import React, {useState} from "react";
-import {ScrollView, View} from "react-native";
-import {Text} from 'react-native-paper';
+import React from "react";
+import {ScrollView} from "react-native";
 import {addWorkStyle} from "../../styles/AddWork";
-import {TextInput} from "react-native-paper";
-import {Picker} from '@react-native-picker/picker';
-import {useQuery} from "react-query";
-import {allWorkGroups} from "../../api/workGroup";
-import DateTimePicker from '@react-native-community/datetimepicker';
+import AddWorkForm from "./AddWorkForm";
+import {FormikHelpers} from "formik";
 
 export default function AddWork(){
 
-    const [date, setDate] = useState(new Date(1598051730000));
-    const [show, setShow] = useState(false);
-
-    const onChange = (event, selectedDate) => {
-        const currentDate = selectedDate || date;
-        setDate(currentDate);
-    };
-
-    const showMode = (currentMode) => {
-        setShow(true);
-    };
-
+    function submit(value: any, helper: FormikHelpers<any>){
+        console.log(value)
+    }
+    
     return (
         <ScrollView style={addWorkStyle.container}>
+            <AddWorkForm 
+                submit={submit}
+                init={{
+                    title: "",
+                    description: "",
+                    groupId: null,
+                    dueDate: new Date()
+                }}
+            />
         </ScrollView>
     )
 }
