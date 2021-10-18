f70eb1bae067a54f9073234a898abbca6cc12ea1
mohammadjavad <mjkmo123@gmail.com>
2021-10-18 09:34:36 +0330

    dashboard

--- core/src/app.service.ts
+++ core/src/app.service.ts
@@ -4,10 +4,23 @@ import {DatabaseService} from "./database/database.service";
 @Injectable()
 export class AppService {
   constructor(private database: DatabaseService) {}
-  getDashboard(userId: number): string {
+  getDashboard(userId: number) {
     return this.database.work.findMany({
       where: {
-
+        OR: [
+          {
+            creatorId: userId,
+          },
+          {
+            group: {
+              subscribers: {
+                some: {
+                  userId: userId,
+                }
+              }
+            }
+          }
+        ]
       }
     });
   }
