bdf70fbaff31608012f7dc100b2319880fb1fc3f
mohammadjavad <mjkmo123@gmail.com>
2021-10-18 09:32:11 +0330

    dashboard

--- core/src/app.controller.ts
+++ core/src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Req, UseGuards} from '@nestjs/common';
 import { AppService } from './app.service';
+import {HttpAuthGuard} from "./auth/http-auth.guard";
+import {Request} from "./types/request";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseGuards(HttpAuthGuard)
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Req() req: Request): string {
+    return this.appService.getDashboard(req.userId);
   }
 }
