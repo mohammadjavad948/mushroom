56dbb9d00d6e9a85bfa33830f93ea54ee2d58991
mohammadjavad <mjkmo123@gmail.com>
2021-10-19 17:12:51 +0330

    - [native]: type safety work

--- mushroomNative/api/work.ts
+++ mushroomNative/api/work.ts
@@ -1,30 +1,31 @@
 import {api} from "./base";
 import {headers} from "./auth";
+import {Work} from "../types";
 
 export function getWork(id: number){
 
-    return api.get('/work/' + id, {
+    return api.get<Work>('/work/' + id, {
         headers: headers()
     })
 }
 
 export function createWork(data){
 
-    return api.post('/work', data, {
+    return api.post<Work>('/work', data, {
         headers: headers()
     })
 }
 
 export function updateWork(id: number, data: any){
 
-    return api.patch('/work/' + id, data, {
+    return api.patch<Work>('/work/' + id, data, {
         headers: headers()
     })
 }
 
 export function removeWork(id: number){
 
-    return api.delete('/work/' + id, {
+    return api.delete<Work>('/work/' + id, {
         headers: headers()
     })
 }
