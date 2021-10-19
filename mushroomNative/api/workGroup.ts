dafac1281af5e02eb586348fa67881c35c8832bb
mohammadjavad <mjkmo123@gmail.com>
2021-10-19 17:13:43 +0330

    - [native]: type safety workgroup

--- mushroomNative/api/workGroup.ts
+++ mushroomNative/api/workGroup.ts
@@ -1,37 +1,38 @@
 import {api} from "./base";
 import {headers} from "./auth";
+import {WorkGroup} from "../types";
 
 export function allWorkGroups(){
 
-    return api.get('/work-group', {
+    return api.get<WorkGroup[]>('/work-group', {
         headers: headers()
     })
 }
 
 export function getWorkGroup(id: number){
 
-    return api.get('/work-group/' + id, {
+    return api.get<WorkGroup>('/work-group/' + id, {
         headers: headers()
     })
 }
 
 export function createWorkGroup(data){
 
-    return api.post('/work-group', data, {
+    return api.post<WorkGroup>('/work-group', data, {
         headers: headers()
     })
 }
 
 export function updateWorkGroup(id: number, data: any){
 
-    return api.patch('/work-group/' + id, data, {
+    return api.patch<WorkGroup>('/work-group/' + id, data, {
         headers: headers()
     })
 }
 
 export function removeWorkGroup(id: number){
 
-    return api.delete('/work-group/' + id, {
+    return api.delete<WorkGroup>('/work-group/' + id, {
         headers: headers()
     })
 }
