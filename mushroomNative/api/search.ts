544215dd5b799c71ebba7770bc795e20d953b8ae
mohammadjavad <mjkmo123@gmail.com>
2021-10-19 17:12:17 +0330

    - [native]: type safety search

--- mushroomNative/api/search.ts
+++ mushroomNative/api/search.ts
@@ -1,9 +1,10 @@
 import {api} from "./base";
 import {headers} from "./auth";
+import {WorkGroup} from "../types";
 
 export function search(query: string){
 
-    return api.get('/search', {
+    return api.get<WorkGroup[]>('/search', {
         headers: headers(),
         params: {
             q: query
