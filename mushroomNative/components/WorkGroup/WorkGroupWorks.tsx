fb6d7bca6ac29aa4e9e989425aec0d1fa672f630
mohammadjavad <mjkmo123@gmail.com>
2021-10-20 10:54:07 +0330

    - [native feature]: workGroupWorks

---
+++ mushroomNative/components/WorkGroup/WorkGroupWorks.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {View} from "react-native";
+
+export default function WorkGroupWorks(){
+
+    return (
+        <View
+            style={{
+                flex: 1
+            }}
+        >
+
+        </View>
+    )
+}
