d49682d84e72c1c7db587efcea9f9c540d6acaaa
mohammadjavad <mjkmo123@gmail.com>
2021-10-20 10:57:13 +0330

    - [native patch]: workGroupWorks params

--- mushroomNative/components/WorkGroup/WorkGroupWorks.tsx
+++ mushroomNative/components/WorkGroup/WorkGroupWorks.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import {View} from "react-native";
+import {useQuery} from "react-query";
+import {getWorkGroup} from "../../api/workGroup";
+import {useParams} from "react-router-native";
 
 export default function WorkGroupWorks(){
+
+    const params = useParams<{id: number}>();
+
+    const {data, isLoading} = useQuery(
+        ['workGroup', {id: params.id}],
+        () => getWorkGroup(params.id)
+    );
 
     return (
         <View
