8080a076c7725e9575418ab41119c6e7717c721b
mohammadjavad <mjkmo123@gmail.com>
2021-10-21 15:08:15 +0330

    - [core test]: workgroup test

---
+++ core/test/work-group.e2e-spec.ts
@@ -0,0 +1,32 @@
+import {Test, TestingModule} from '@nestjs/testing';
+import {INestApplication, VersioningType} from '@nestjs/common';
+import * as request from 'supertest';
+import {AppModule} from './../src/app.module';
+import {user, version} from "./data";
+
+describe('work group (e2e)', () => {
+    let app: INestApplication;
+    let token = "";
+
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+
+        app.enableVersioning({
+            type: VersioningType.URI,
+            defaultVersion: '1',
+        });
+
+        await app.init();
+
+        request(app.getHttpServer())
+            .post(`/${version}/auth/login`)
+            .send(user)
+            .then((data) => {
+                token = data.token
+            })
+    });
+});
