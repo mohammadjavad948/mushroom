217f7b2aa1be16792577cbee246102580efa4ef5
mohammadjavad <mjkmo123@gmail.com>
2021-10-22 18:43:36 +0330

    - [core db]: seed

---
+++ core/prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client'
+import {hash} from "bcrypt";
+const prisma = new PrismaClient()
+
+async function main() {
+
+    const user = await prisma.user.create({
+        data: {
+            username: "test",
+            password: await hash("test", 10),
+            isVerified: true,
+            canCreatePublicWorkGroup: true
+        }
+    });
+
+    console.log({ user })
+}
+
+main()
+    .catch((e) => {
+        console.error(e)
+        process.exit(1)
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
