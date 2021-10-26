ea72c6725bf300011029f461074980c29e73dec9
mohammadjavad <mjkmo123@gmail.com>
2021-10-26 14:53:54 +0330

    - [native i18n] fa locale

--- mushroomNative/locale/init.ts
+++ mushroomNative/locale/init.ts
@@ -2,6 +2,7 @@ import i18next from "i18next";
 import {initReactI18next} from "react-i18next";
 
 import en from './en/default.json';
+import fa from './fa/default.json';
 
 i18next
     .use(initReactI18next)
@@ -9,10 +10,13 @@ i18next
         resources: {
             en: {
                 translation: en
+            },
+            fa: {
+                translation: fa
             }
         },
-        lng: "en", // if you're using a language detector, do not define the lng option
-        fallbackLng: "en",
+        lng: "fa", // if you're using a language detector, do not define the lng option
+        fallbackLng: "fa",
         interpolation: {
             escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
         }
