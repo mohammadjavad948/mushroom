eddd3e913e68b31595762076a89e2523e2371a9d
mohammadjavad <mjkmo123@gmail.com>
2021-10-28 11:48:42 +0330

    - [core feature] guard

--- core/src/pins/pins.controller.ts
+++ core/src/pins/pins.controller.ts
@@ -1,7 +1,9 @@
-import {Controller, Delete, Post} from '@nestjs/common';
+import {Controller, Delete, Post, UseGuards} from '@nestjs/common';
 import { PinsService } from './pins.service';
+import {HttpAuthGuard} from "../auth/http-auth.guard";
 
 @Controller('pins')
+@UseGuards(HttpAuthGuard)
 export class PinsController {
   constructor(private readonly pinsService: PinsService) {}
 
