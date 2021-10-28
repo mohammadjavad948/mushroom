9e823d4b0a8f662149b7f86979097e9ede6ab3b3
mohammadjavad <mjkmo123@gmail.com>
2021-10-28 11:41:58 +0330

    - [core feature] pin

--- core/src/pins/pins.controller.ts
+++ core/src/pins/pins.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Delete, Post} from '@nestjs/common';
 import { PinsService } from './pins.service';
 
 @Controller('pins')
 export class PinsController {
   constructor(private readonly pinsService: PinsService) {}
+
+  @Post(':id')
+  async pin(){
+
+  }
+
+  @Delete(':id')
+  async unpin(){
+
+  }
 }
