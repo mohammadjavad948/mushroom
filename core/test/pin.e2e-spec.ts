a30e0c06c5c29d7e28828d41acd2b0963c4073b6
mohammadjavad <mjkmo123@gmail.com>
2021-10-30 14:12:36 +0330

    - [core fix] bug fix

--- core/test/pin.e2e-spec.ts
+++ core/test/pin.e2e-spec.ts
@@ -40,7 +40,7 @@ describe('pins (e2e)', () => {
   it('all pins', async () => {
     const data = await request(app.getHttpServer())
         .get(`/${version}/pins/1`)
-        .send(user);
+        .set('auth', token)
 
     expect(data.statusCode).toEqual(200);
     expect(data.body.length).toEqual(1);
