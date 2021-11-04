3eeb96591c8c3dae28d8bb762101e478a024d475
mohammadjavad <mjkmo123@gmail.com>
2021-11-04 19:10:11 +0330

    - [native type] types

--- mushroomNative/types/index.ts
+++ mushroomNative/types/index.ts
@@ -1,10 +1,15 @@
-export interface User {
-  id: number;
-  username: string;
-  password: string;
-  isVerified: boolean;
-  canCreatePublicWorkGroup: boolean;
-  createdAt: string;
+
+/**
+ * Model User
+ */
+
+export type User = {
+  id: number
+  username: string
+  password: string
+  isVerified: boolean
+  canCreatePublicWorkGroup: boolean
+  createdAt: string
 
   workGroups?: WorkGroup[];
   works?: Work[];
@@ -12,30 +17,42 @@ export interface User {
 }
 
 /**
+ * Model Session
+ */
+
+export type Session = {
+  id: number
+  userId: number
+  hash: string
+  createdAt: string
+}
+
+/**
  * Model WorkGroup
  */
 
-export interface WorkGroup {
-  id: number;
-  creatorId: number;
-  name: string;
-  color: string;
-  info: string;
-  haveComments: boolean;
-  isVerified: boolean;
-  isPrivate: boolean;
-  createdAt: string;
-  updatedAt: string;
+export type WorkGroup = {
+  id: number
+  creatorId: number
+  name: string
+  color: string
+  info: string
+  haveComments: boolean
+  isVerified: boolean
+  isPrivate: boolean
+  createdAt: string
+  updatedAt: string
 
   works?: Work[];
   subscribers?: Subscription[];
+  pins?: Pin[]
 }
 
 /**
  * Model Subscription
  */
 
-export interface Subscription {
+export type Subscription = {
   id: number;
   userId: number;
   user?: User;
@@ -48,15 +65,30 @@ export interface Subscription {
  * Model Work
  */
 
-export interface Work {
-  id: number;
-  groupId: number;
-  group?: WorkGroup;
-  creatorId: number;
-  creator?: User;
-  title: string;
-  description: string;
-  dueDate: string;
-  createdAt: string;
-  updatedAt: string;
+export type Work = {
+  id: number
+  groupId: number
+  group?: WorkGroup
+  creatorId: number
+  creator?: User
+  title: string
+  description: string
+  dueDate: string
+  createdAt: string
+  updatedAt: string
+
+  pins?: Pin[]
+}
+
+/**
+ * Model Pin
+ */
+
+export type Pin = {
+  id: number
+  workId: number
+  work?: Work
+  workGroupId: number
+  workGroup?: WorkGroup
+  createdAt: string
 }
