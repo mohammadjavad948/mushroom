b1fe934f6a3249f400757ef4b0ee066201193243
mohammadjavad <mjkmo123@gmail.com>
2021-11-04 19:10:40 +0330

    - [native api] types

--- mushroomNative/api/pins.ts
+++ mushroomNative/api/pins.ts
@@ -1,9 +1,11 @@
 import {api} from "./base";
 import {headers} from "./auth";
+import {Pin} from "../types";
+import {AxiosResponse} from "axios";
 
 export function allPins(groupId: number){
 
-    return api.get(`/pins/${groupId}`, {
+    return api.get<Pin[], AxiosResponse<Pin[]>>(`/pins/${groupId}`, {
         headers: headers()
     })
 }
