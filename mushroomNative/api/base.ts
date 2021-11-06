c1481f4c9851f6f78ee255fe68ea657330d1866c
mohammadjavad <mjkmo123@gmail.com>
2021-11-06 17:30:54 +0330

    - [native fix] export api

--- mushroomNative/api/base.ts
+++ mushroomNative/api/base.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: __DEV__ ? 'http://localhost:3000/v1' : 'https://big-mushroom.herokuapp.com/v1',
 });
+
+
+
+export {api}
