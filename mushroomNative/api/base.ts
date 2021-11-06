c926eb7a203445a66cd875f620e31efed69492af
mohammadjavad <mjkmo123@gmail.com>
2021-11-06 17:36:08 +0330

    - [native api] add interceptor

--- mushroomNative/api/base.ts
+++ mushroomNative/api/base.ts
@@ -1,9 +1,43 @@
 import axios from 'axios';
+import {ToastAndroid} from "react-native";
 
 const api = axios.create({
   baseURL: __DEV__ ? 'http://localhost:3000/v1' : 'https://big-mushroom.herokuapp.com/v1',
 });
 
+
+api.interceptors.response.use((response) => response, (error) => {
+
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+
+    ToastAndroid.showWithGravity(
+        `Error ${error.response.status}`,
+        ToastAndroid.SHORT,
+        ToastAndroid.CENTER
+    );
+
+  } else if (error.request) {
+    // The request was made but no response was received
+
+    ToastAndroid.showWithGravity(
+        "No Response Received",
+        ToastAndroid.SHORT,
+        ToastAndroid.CENTER
+    );
+  } else {
+    // Something happened in setting up the request that triggered an Error
+
+    ToastAndroid.showWithGravity(
+        "Unknown Error",
+        ToastAndroid.SHORT,
+        ToastAndroid.CENTER
+    );
+  }
+
+  throw error;
+});
 
 
 export {api}
