c9401698f4393294bedc8b8d14125adbc891097b
mohammadjavad <mjkmo123@gmail.com>
2021-11-10 13:34:05 +0330

    - [web O_O] hmm

--- web/components/layout/Main.tsx
+++ web/components/layout/Main.tsx
@@ -4,7 +4,7 @@ import style from '../../styles/main.module.css';
 export default function MainLayout(props: {children?: any}){
 
     return (
-        <div>
+        <div className={style.container}>
 
         </div>
     )
