b483ddebcef8f91245b58fc79d054c1e8ca5455b
mohammadjavad <mjkmo123@gmail.com>
2021-11-10 13:50:31 +0330

    - [web O_O] better

--- web/components/layout/Main.tsx
+++ web/components/layout/Main.tsx
@@ -6,8 +6,8 @@ export default function MainLayout(props: {children?: any}){
 
     return (
         <div className={style.container}>
-            <SideBar />
             {props.children}
+            <SideBar />
         </div>
     )
 }
