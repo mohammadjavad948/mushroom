99dfe8ca27549db3ada63a98198b0d405afc2dc9
mohammadjavad <mjkmo123@gmail.com>
2021-11-10 13:58:45 +0330

    - [web O_O] icon

--- web/components/sideBar/SideBarIcon.tsx
+++ web/components/sideBar/SideBarIcon.tsx
@@ -1,10 +1,10 @@
 import style from './sideBarIcon.module.css';
 
-export default function SideBarIcon(){
+export default function SideBarIcon(props: {children: any}){
 
     return (
         <div className={style.container}>
-
+            {props.children}
         </div>
     )
 }
