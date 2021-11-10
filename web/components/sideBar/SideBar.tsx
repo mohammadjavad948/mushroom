26265a9b8077ca84b0f8d358a0d0a4cfbbb567de
mohammadjavad <mjkmo123@gmail.com>
2021-11-10 14:04:07 +0330

    - [web O_O] good

--- web/components/sideBar/SideBar.tsx
+++ web/components/sideBar/SideBar.tsx
@@ -1,10 +1,14 @@
 import style from './sideBar.module.css';
+import SideBarIcon from "./SideBarIcon";
+import {MdDashboard} from 'react-icons/md';
 
 export default function SideBar(){
 
     return (
         <div className={style.container}>
-            nice
+            <SideBarIcon>
+                <MdDashboard size={25} />
+            </SideBarIcon>
         </div>
     )
 }
