d8b0dc6e01f9bf628338da5e335ef4e7a16b94f2
mohammadjavad <mjkmo123@gmail.com>
2021-11-11 09:07:35 +0330

    - [web] progress

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css';
 import {QueryClient, QueryClientProvider} from "react-query";
+import NextNProgress from 'nextjs-progressbar';
 
 const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <QueryClientProvider client={queryClient}>
+        <NextNProgress />
         <Component {...pageProps} />
       </QueryClientProvider>
   )
