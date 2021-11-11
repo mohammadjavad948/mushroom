1bf28149a9664a6870015aaec470d4a36619dc49
mohammadjavad <mjkmo123@gmail.com>
2021-11-11 15:17:12 +0330

    - [web] remove persist

--- web/components/layout/Main.tsx
+++ web/components/layout/Main.tsx
@@ -1,19 +1,23 @@
 import style from '../../styles/main.module.css';
 import SideBar from "../sideBar/SideBar";
 import HelperPage from "../helperPage/HelperPage";
+import {useOtherInfoStore} from "../../store/OtherInfoStore";
 
 
 export default function MainLayout(props: {children?: any}){
 
+    const {helperPageEnable} = useOtherInfoStore();
 
     return (
         <div className={style.container}>
             <div style={{direction: 'rtl'}} className={style.DataContainer}>
                 {props.children}
             </div>
-            <HelperPage>
-                hmm
-            </HelperPage>
+            {helperPageEnable && (
+                <HelperPage>
+                    hmm
+                </HelperPage>
+            )}
             <SideBar />
         </div>
     )
