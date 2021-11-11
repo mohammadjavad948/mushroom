cd22afb71ad41afeb91e89dc9a56662f8a4d7947
mohammadjavad <mjkmo123@gmail.com>
2021-11-11 09:00:09 +0330

    - [web fix] title

--- web/components/title/PageTitle.tsx
+++ web/components/title/PageTitle.tsx
@@ -1,8 +1,12 @@
+import {Head} from "next/document";
 
-export default function PageTitle(props: {children: any}){
+export default function PageTitle(props: {children: string}){
 
     return (
         <div style={{width: '100%'}}>
+            <Head>
+                <title>{props.children}</title>
+            </Head>
             <h3>{props.children}</h3>
         </div>
     )
