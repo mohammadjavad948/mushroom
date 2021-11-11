8ad72d025c0eeb18e3e44f30f32b8a5f16eb77cd
mohammadjavad <mjkmo123@gmail.com>
2021-11-11 14:03:50 +0330

    - [web bug]

--- web/components/helperPage/HelperPage.tsx
+++ web/components/helperPage/HelperPage.tsx
@@ -1,10 +1,31 @@
 import style from './helperPage.module.css';
+import {useDrag} from "@use-gesture/react";
+import {useOtherInfoStore} from "../../store/OtherInfoStore";
+import {useSpring, a} from "react-spring";
 
 export default function HelperPage(props: {children: any}){
 
+    const [animation, api] = useSpring(() => {
+        return {
+            x: 0,
+        }
+    })
+
+    const bind = useDrag(({offset}) => {
+        api.start({
+            x: offset[0]
+        })
+    }) as any;
+
     return (
-        <div className={style.container}>
-            <div className={style.smallDraggable} />
+        <div
+            className={style.container}
+        >
+            <a.div
+                className={style.smallDraggable}
+                style={animation}
+                {...bind()}
+            />
             {props.children}
         </div>
     )
