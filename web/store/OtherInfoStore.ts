6d00c59800de9c929691c17042833ad1043ec8ba
mohammadjavad <mjkmo123@gmail.com>
2021-11-11 14:36:31 +0330

    - [web] nice

--- web/store/OtherInfoStore.ts
+++ web/store/OtherInfoStore.ts
@@ -3,6 +3,10 @@ import {persist, StateStorage} from 'zustand/middleware';
 
 interface OtherInfoStoreI {
     helperPageWidth: number;
+    helperPageEnable: boolean;
+    openHelperPage: () => void;
+    closeHelperPage: () => void;
+    toggleHelperPage: () => void;
     setHelperPageWidth: (width: number) => void;
 }
 
@@ -11,6 +15,18 @@ export const useOtherInfoStore = create<OtherInfoStoreI>(
         (set, get) => {
             return {
                 helperPageWidth: 400,
+                helperPageEnable: false,
+                openHelperPage: () => {
+                  set({helperPageEnable: true})
+                },
+                closeHelperPage: () => {
+                    set({helperPageEnable: false})
+                },
+                toggleHelperPage: () => {
+                  const state = get().helperPageEnable;
+
+                  set({helperPageEnable: !state})
+                },
                 setHelperPageWidth: width => {
                     set({helperPageWidth: width});
                 },
