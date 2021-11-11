6ff64697318fc4e84f07c4d1b5edb28052b87e4e
mohammadjavad <mjkmo123@gmail.com>
2021-11-11 09:46:16 +0330

    - [web] hmm

--- web/components/sideBar/SideBar.tsx
+++ web/components/sideBar/SideBar.tsx
@@ -3,18 +3,34 @@ import SideBarIcon from "./SideBarIcon";
 import {MdDashboard, MdLayers} from 'react-icons/md';
 import {useRouter} from "next/router";
 
+const ITEMS = [
+    {
+        route: '/',
+        icon: <MdDashboard size={25} />,
+    },
+    {
+        route: '/workgroup',
+        icon: <MdLayers size={25} />,
+    }
+]
+
 export default function SideBar(){
 
     const router = useRouter();
 
     return (
         <div className={style.container}>
-            <SideBarIcon isActive={true} click={() => router.push('/')}>
-                <MdDashboard size={25} />
-            </SideBarIcon>
-            <SideBarIcon isActive={false} click={() => router.push('/workgroup')}>
-                <MdLayers size={25} />
-            </SideBarIcon>
+            {ITEMS.map((el, i) => {
+                return (
+                    <SideBarIcon
+                        key={i}
+                        isActive={router.asPath.startsWith(el.route)}
+                        click={() => router.push(el.route)}
+                    >
+                        {el.icon}
+                    </SideBarIcon>
+                )
+            })}
         </div>
     )
 }
