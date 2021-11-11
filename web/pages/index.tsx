671759fc40e9deb62750ca185d8ab297de12583a
mohammadjavad <mjkmo123@gmail.com>
2021-11-11 07:49:20 +0330

    - [web O_O] add zustand + react spring

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Main(){
         <MainLayout>
             <div className="row">
                 <div className="col-12">
-                    hmm?
+                    
                 </div>
             </div>
         </MainLayout>
