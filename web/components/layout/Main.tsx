fd5c1e31e16b6efcbc28ecd90ec45d5c851aaea9
mohammadjavad <mjkmo123@gmail.com>
2021-11-11 13:38:29 +0330

    - [web fix] nice

--- web/components/layout/Main.tsx
+++ web/components/layout/Main.tsx
@@ -1,18 +1,23 @@
 import style from '../../styles/main.module.css';
 import SideBar from "../sideBar/SideBar";
 import HelperPage from "../helperPage/HelperPage";
+import {useMediaQuery} from "@react-hook/media-query";
 
 
 export default function MainLayout(props: {children?: any}){
+
+    const match = useMediaQuery('only screen and (max-width: 600px)')
 
     return (
         <div className={style.container}>
             <div style={{direction: 'rtl'}} className={style.DataContainer}>
                 {props.children}
             </div>
-            <HelperPage>
-                hmm
-            </HelperPage>
+            {match && (
+                <HelperPage>
+                    hmm
+                </HelperPage>
+            )}
             <SideBar />
         </div>
     )
