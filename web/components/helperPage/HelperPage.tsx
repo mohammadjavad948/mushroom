a9af2d314a3ea5ed2e025d59aa82ec3a4ab4cb77
mohammadjavad <mjkmo123@gmail.com>
2021-11-11 14:27:24 +0330

    - [web bug] god damn it finally

--- web/components/helperPage/HelperPage.tsx
+++ web/components/helperPage/HelperPage.tsx
@@ -5,21 +5,34 @@ import pageStyle from './HelperPage.module.css';
 
 export default function HelperPage(props: {children: any}){
 
+    const {helperPageWidth, setHelperPageWidth} = useOtherInfoStore();
+
     const [animation, api] = useSpring(() => {
         return {
             x: 0,
         }
-    })
+    });
+
+    const mainAnimation = useSpring({
+        width: helperPageWidth
+    } as any) as any;
 
     const bind = useDrag(({movement: [mx, my], down}) => {
         api.start({
             x: down ? mx : 0
-        })
+        });
+
+        if (!down){
+            setHelperPageWidth(helperPageWidth + (-1 * mx));
+        }
     }) as any;
 
     return (
-        <div
+        <a.div
             className={pageStyle.container}
+            style={{
+                width: mainAnimation.width.to((x: any) => `${x}px`)
+            }}
         >
             <a.div
                 className={pageStyle.smallDraggable}
@@ -27,6 +40,6 @@ export default function HelperPage(props: {children: any}){
                 {...bind()}
             />
             {props.children}
-        </div>
+        </a.div>
     )
 }
