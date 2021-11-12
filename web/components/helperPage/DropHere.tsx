27e8d387e3958ff63166869a14e60cfc9f627da7
mohammadjavad <mjkmo123@gmail.com>
2021-11-12 09:58:45 +0330

    - [web] nice

--- web/components/helperPage/DropHere.tsx
+++ web/components/helperPage/DropHere.tsx
@@ -1,16 +1,25 @@
 import style from './HelperPage.module.css';
+import {useSpring, a} from "react-spring";
+import {useOtherInfoStore} from "../../store/OtherInfoStore";
 
 export default function DropComponentHere(){
 
+    const {isDragging} = useOtherInfoStore();
+
+    const animation = useSpring({
+        backgroundColor: !isDragging ? 'rgba(255, 255, 255, 0)' : 'rgba(21, 255, 255, 0.43)',
+    } as any) as any;
+
     return (
-        <div
+        <a.div
             onMouseOver={() => console.log('hmm')}
             onMouseUpCapture={() => console.log('capture')}
             className={style.dropHere}
+            style={animation}
         >
             <h3>
                 صفحه را اینجا بیاندازید
             </h3>
-        </div>
+        </a.div>
     )
 }
