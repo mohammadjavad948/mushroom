55ccf57ff1250d3c41eb4a70ad14a48ca14f15c0
mohammadjavad <mjkmo123@gmail.com>
2021-11-12 10:09:02 +0330

    - [web] nice

--- web/components/ComponentInfo.tsx
+++ web/components/ComponentInfo.tsx
@@ -1,8 +1,10 @@
 import MainWorkGroup from "./workgroup/main";
+import MainDashboard from "./dashboard";
 
 export const componentInfo = [
     {
         id: 'dashboard',
+        component: <MainDashboard />
     },
     {
         id: 'workgroups',
