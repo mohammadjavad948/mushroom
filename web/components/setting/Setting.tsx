571040c024a6d27a28f70422bcf4404e02c1d67e
mohammadjavad <mjkmo123@gmail.com>
2021-11-18 14:05:19 +0330

    - [web] setting

--- web/components/setting/Setting.tsx
+++ web/components/setting/Setting.tsx
@@ -1,7 +1,10 @@
+import PageTitle from "../title/PageTitle";
 
 export default function MainSetting(){
 
     return (
-
+        <div className="row">
+            <PageTitle title={"تنظیمات"} />
+        </div>
     )
 }
