8fea2c84fe5b283a285c10e16917ad7f640c57ed
mohammadjavad <mjkmo123@gmail.com>
2021-11-21 11:39:23 +0330

    - [web api] base url

--- web/api/base.ts
+++ web/api/base.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: true ? 'http://localhost:3000/v1' : 'https://big-mushroom.herokuapp.com/v1',
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/v1' : 'https://big-mushroom.herokuapp.com/v1',
 });
 
 
