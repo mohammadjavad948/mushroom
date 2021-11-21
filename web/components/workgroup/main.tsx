d12f6b60334bee7cc731eb1e061eabcd00a24250
mohammadjavad <mjkmo123@gmail.com>
2021-11-21 14:06:30 +0330

    - [web] button

--- web/components/workgroup/main.tsx
+++ web/components/workgroup/main.tsx
@@ -1,10 +1,15 @@
 import PageTitle from "../title/PageTitle";
+import Button from "../form/Button";
 
 export default function MainWorkGroup(){
 
     return (
         <div className="row">
-            <PageTitle title={"گروه"} />
+            <PageTitle title={"گروه"}>
+                <Button>
+                    گروه جدید
+                </Button>
+            </PageTitle>
         </div>
     )
 }
