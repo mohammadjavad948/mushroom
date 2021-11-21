24914f2848577b6b640c9e355df1350dec44cab1
mohammadjavad <mjkmo123@gmail.com>
2021-11-21 14:17:01 +0330

    - [web] fix

--- web/components/workgroup/main.tsx
+++ web/components/workgroup/main.tsx
@@ -1,15 +1,19 @@
 import PageTitle from "../title/PageTitle";
 import Button from "../form/Button";
+import WorkGroupItem from "./WorkGroupItem";
 
 export default function MainWorkGroup(){
 
     return (
-        <div className="row">
+        <div className="d-flex" style={{flexWrap: 'wrap'}}>
             <PageTitle title={"گروه"}>
                 <Button>
                     گروه جدید
                 </Button>
             </PageTitle>
+            <div style={{width: '100%'}}>
+                <WorkGroupItem />
+            </div>
         </div>
     )
 }
