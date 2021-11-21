31876f4a141ff2c74136b1b9efc2bbbfd947a4b4
mohammadjavad <mjkmo123@gmail.com>
2021-11-21 14:18:10 +0330

    - [web] bug fix

--- web/components/dashboard/index.tsx
+++ web/components/dashboard/index.tsx
@@ -4,8 +4,8 @@ import WorkItem from "../Work/WorkItem";
 export default function MainDashboard(){
 
     return (
-        <div className="row">
-            <div className="col-12">
+        <div className="d-flex" style={{flexWrap: 'wrap'}}>
+            <div style={{width: '100%'}}>
                 <PageTitle title={"داشبورد"} />
                 <WorkItem />
             </div>
