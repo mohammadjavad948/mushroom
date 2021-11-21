0795be1a8c2c3c1bf71de421e6c1f74ae79fccfb
mohammadjavad <mjkmo123@gmail.com>
2021-11-21 14:05:52 +0330

    - [web] child

--- web/components/title/PageTitle.tsx
+++ web/components/title/PageTitle.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head";
 
-export default function PageTitle(props: {title: string}){
+export default function PageTitle(props: {title: string, children?: any}){
 
     return (
-        <div style={{width: '100%'}}>
+        <div style={{width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
             <Head>
                 <title>{props.title}</title>
             </Head>
             <h3>{props.title}</h3>
+            {props.children}
         </div>
     )
 }
