f3b71eac5bc346ebbb90a168a1702736cbd21bb4
mohammadjavad <mjkmo123@gmail.com>
2021-11-22 17:24:10 +0330

    - [web] border

--- web/components/workgroup/WorkGroupItem.tsx
+++ web/components/workgroup/WorkGroupItem.tsx
@@ -4,7 +4,7 @@ import {WorkGroup} from "../../types";
 export default function WorkGroupItem(props: {data: WorkGroup}){
 
     return (
-        <div className={style.main}>
+        <div className={style.main} style={{borderColor: props.data.color}}>
             {props.data.name}
         </div>
     )
