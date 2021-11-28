738b37a155d3774cd4234004cf16375af0aa486b
mohammadjavad <mjkmo123@gmail.com>
2021-11-28 14:27:05 +0330

    - [web] nice

--- web/pages/workgroup/[id].tsx
+++ web/pages/workgroup/[id].tsx
@@ -3,18 +3,22 @@ import MainLayout from "../../components/layout/Main";
 import PageTitle from "../../components/title/PageTitle";
 import {useRouter} from "next/router";
 import {useQuery} from "react-query";
+import {getWorkGroup} from "../../api/workGroup";
 
 export default function WorkGroup(){
 
     const router = useRouter();
 
-    const {} = useQuery(['workgroups'])
+    const {data, isLoading} = useQuery(
+        ['workgroups', {id: router.query.id}],
+        () => getWorkGroup(router.query.id as number)
+    )
 
     return (
         <AuthWrapper>
             <MainLayout>
                 <div className="d-flex" style={{flexWrap: 'wrap'}}>
-                    <PageTitle title={"hmm"} />
+                    <PageTitle title={isLoading ? "loading..." : data?.data.name} />
                 </div>
             </MainLayout>
         </AuthWrapper>
