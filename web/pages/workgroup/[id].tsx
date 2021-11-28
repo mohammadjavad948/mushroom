9069fb0135fc7f5936470073d05d33b491ff8ec0
mohammadjavad <mjkmo123@gmail.com>
2021-11-28 14:39:48 +0330

    - [web] nice

--- web/pages/workgroup/[id].tsx
+++ web/pages/workgroup/[id].tsx
@@ -4,6 +4,7 @@ import PageTitle from "../../components/title/PageTitle";
 import {useRouter} from "next/router";
 import {useQuery} from "react-query";
 import {getWorkGroup} from "../../api/workGroup";
+import WorkItem from "../../components/Work/WorkItem";
 
 export default function WorkGroup(){
 
@@ -19,6 +20,14 @@ export default function WorkGroup(){
             <MainLayout>
                 <div className="d-flex" style={{flexWrap: 'wrap'}}>
                     <PageTitle title={isLoading ? "loading..." : data?.data.name} />
+
+                    <div style={{width: '100%', display: 'flex', flexWrap: 'wrap'}}>
+                        {data?.data.works?.map((e, i) => {
+                            return (
+                                <WorkItem key={i}/>
+                            )
+                        })}
+                    </div>
                 </div>
             </MainLayout>
         </AuthWrapper>
