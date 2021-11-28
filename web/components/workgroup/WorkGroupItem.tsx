79b7319e406422f59bd58313d36168bec4f285da
mohammadjavad <mjkmo123@gmail.com>
2021-11-28 14:14:22 +0330

    - [web] route

--- web/components/workgroup/WorkGroupItem.tsx
+++ web/components/workgroup/WorkGroupItem.tsx
@@ -1,10 +1,21 @@
 import style from './workGroupItem.module.css';
 import {WorkGroup} from "../../types";
+import {useRouter} from "next/router";
 
 export default function WorkGroupItem(props: {data: WorkGroup}){
 
+    const router = useRouter();
+
+    function click(){
+        router.push('/workgroup/' + props.data.id)
+    }
+
     return (
-        <div className={style.main} style={{borderColor: props.data.color}}>
+        <div
+            className={style.main}
+            style={{borderColor: props.data.color}}
+            onClick={click}
+        >
             {props.data.name}
         </div>
     )
