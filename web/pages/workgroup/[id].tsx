77cb0aa649d257e798f35cc07ecbf06944a5bfa1
mohammadjavad <mjkmo123@gmail.com>
2021-11-28 14:25:08 +0330

    - [web] router

--- web/pages/workgroup/[id].tsx
+++ web/pages/workgroup/[id].tsx
@@ -1,13 +1,20 @@
 import AuthWrapper from "../../components/auth/AuthWrapper";
 import MainLayout from "../../components/layout/Main";
+import PageTitle from "../../components/title/PageTitle";
+import {useRouter} from "next/router";
+import {useQuery} from "react-query";
 
 export default function WorkGroup(){
+
+    const router = useRouter();
+
+    const {} = useQuery(['workgroups'])
 
     return (
         <AuthWrapper>
             <MainLayout>
-                <div>
-                    hm
+                <div className="d-flex" style={{flexWrap: 'wrap'}}>
+                    <PageTitle title={"hmm"} />
                 </div>
             </MainLayout>
         </AuthWrapper>
